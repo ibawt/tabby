e4232393f17e6e52139138af9502ddeef92f08f9
Ian Quick <ian.quick@gmail.com>
2015-11-13 15:02:59 -0500

    clean up follower

--- src/tabby/follower.clj
+++ src/tabby/follower.clj
@@ -1,9 +1,9 @@
 (ns tabby.follower
-  (:require [clojure.tools.logging :refer :all]
-            [tabby.candidate :refer :all]
+  (:require [clojure.tools.logging :refer [warn info]]
+            [tabby.candidate :as candidate]
             [tabby.client-state :as cs]
-            [tabby.log :refer :all]
-            [tabby.utils :refer :all]))
+            [tabby.log :as log]
+            [tabby.utils :as utils]))
 
 (defn- election-timeout? [state]
   (<= (:election-timeout state) 0))
@@ -11,9 +11,9 @@
 (defn- request-vote [state params]
   (let [r {:term (:current-term state)
            :vote-granted?
-           (and (valid-term? state params)
+           (and (utils/valid-term? state params)
                 (not (:voted-for state))
-                (prev-log-term-equals? state params))}]
+                (log/prev-log-term-equals? state params))}]
     {:response r
      :state (if (:vote-granted? r)
               (assoc state :voted-for (:candidate-id params))
@@ -23,35 +23,35 @@
 (defn- append-entries [state params]
   (let [r {:term (:current-term state)
            :count (count (:entries params))
-           :success (and (valid-term? state params)
-                         (prev-log-term-equals? state params))}]
-    {:state (if (:success r) (append-log state params) state)
+           :success (and (utils/valid-term? state params)
+                         (log/prev-log-term-equals? state params))}]
+    {:state (if (:success r) (log/append-log state params) state)
      :result r}))
 
 (defn handle-request-vote
   "incoming request to for a vote packet"
   [state p]
   (let [{s :state r :response} (request-vote state (:body p))]
-    (transmit s  {:dst (:src p)
-                  :src (:id state)
-                  :type :request-vote-reply
-                  :body r})))
+    (utils/transmit s {:dst (:src p)
+                       :src (:id state)
+                       :type :request-vote-reply
+                       :body r})))
 
 (defn check-election-timeout
   "checks if we have timed out on the election,
    and transitions into candidate state"
   [state]
   (if (election-timeout? state)
-    (become-candidate state)
+    (candidate/become-candidate state)
     state))
 
 (defn handle-append-entries
   "append entries packet"
   [state p]
   (let [r (append-entries state (:body p))]
-    (transmit (:state r) {:dst (:src p) :src (:id state)
-                          :type :append-entries-response
-                          :body (:result r)})))
+    (utils/transmit (:state r) {:dst (:src p) :src (:id state)
+                                :type :append-entries-response
+                                :body (:result r)})))
 
 (defn become-follower
   "called from the packet queue if current-term < supplied term"
@@ -61,7 +61,7 @@
       (cs/close-clients)
       (dissoc :next-timeout)
       (dissoc :match-index)
-      (assoc :election-timeout (random-election-timeout))
+      (assoc :election-timeout (utils/random-election-timeout))
       (assoc :leader-id leader-id)
       (assoc :type :follower)
       (dissoc :voted-for)))
