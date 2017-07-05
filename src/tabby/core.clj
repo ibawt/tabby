afaed0933a7b1f648f8403bbfcd929970027a0ae
Ian Quick <ian.quick@gmail.com>
2017-07-04 23:21:33 -0400

    stuff

--- src/tabby/core.clj
+++ src/tabby/core.clj
@@ -2,49 +2,35 @@
   (:require [tabby.cluster :as cluster]
             [tabby.local-net :as local-net]
             [clojure.tools.logging :refer [warn info]]
+            [clojure.tools.cli :as cli]
             [tabby.net :as net]
             [tabby.server :as server]
             [tabby.utils :as u])
   (:gen-class))
 
-(def test-args
-  ["-port" "1337" "-peers" "foo:1,foo:2,foo:3"])
+(defn- parse-peers [s]
+  (map (fn [x]
+         (let [[_ host port id] (re-matches #"(.*):(\d+)=(\d+)" x)]
+           {:host host :port (Integer/parseInt port) :id id}))
+       (.split s ",")))
 
-(def expected
-  {:port 1337
-   :peers [{:hostname "foo" :port 1}
-           {:hostname "foo" :port 2}
-           {:hostname "foo" :port 3}]})
-
-(defn- parse-int [x]
-  (Integer/parseInt x))
-
-(defn- parse-peers [^String s]
-  (map (fn [^String x]
-         (let [[hostname port] (.split x ":")]
-           {:hostname hostname
-            :id x
-            :port (parse-int port)})) (.split s ",")))
-
-(defn- convert [options]
-  (-> options
-      (update :port parse-int)
-      (update :peers parse-peers)))
+(def cli-options
+  [["-p" "--port PORT" "Port number"
+   :default 8080
+    :parse-fn #(Integer/parseInt %)
+    :validate-fn [#(and (> % 1024) (<  % 65535 ))]]
+   ["-i" "--id ID" "Unique ID"]
+   ["-P" "--peers PEERLIST"
+    :parse-fn parse-peers]])
 
 (defn- parse-args [args]
-  (loop [args args
-         opts {}]
-    (if (empty? args)
-      opts
-      (let [^String a (first args)]
-        (if (= (first a) \-)
-          (recur (rest (rest args))
-                 (assoc opts (keyword (.substring a 1)) (second args))))))))
+  (let [opts (cli/parse-opts args cli-options)]
+    (:options opts)))
 
 (defn -main [& args]
   (try
     (let [options (parse-args args)]
-     (-> (server/create-server (str (:hostname options) ":" (:port options)))
+     (-> (server/create-server (:id options))
          (net/start-server! (:port options))
          (server/set-peers (:peers options))))
     (catch Exception e
