d372b809e2b8ac03f59e50295d29e86ac412621d
Ian Quick <ian.quick@gmail.com>
2018-01-06 09:39:55 -0500

    update deps

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/ibawt/tabby"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [compojure "1.6.0"]
                  [ring/ring-json "0.4.0"]
-                 [clj-http "3.6.1"]
-                 [cheshire "5.7.1"]
-                 [aleph "0.4.3"]
+                 [clj-http "3.7.0"]
+                 [cheshire "5.8.0"]
+                 [aleph "0.4.4"]
                  [gloss "0.2.6"]
-                 [com.taoensso/nippy "2.13.0"]
+                 [com.taoensso/nippy "2.14.0"]
                  [log4j "1.2.17" :exclusions [javax.mail/mail
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
@@ -20,8 +20,6 @@
   :repl-options {:init-ns user}
   :profiles {:uberjar {:aot :all}
              :dev {:plugins [[lein-cloverage "1.0.9"]]
-                   :dependencies [
-                                  [org.clojure/tools.namespace "0.2.11"]]
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["dev"]}
-             :test {:dependencies [
-                                   [lein-cloverage "1.0.9"]]}})
+             :test {:dependencies [[lein-cloverage "1.0.10"]]}})
